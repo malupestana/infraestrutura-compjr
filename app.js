967011e9c1efbae2dfbeaaa913961ef753f4ef54
Maria Luiza Pestana <pestanamaria099@gmail.com>
2025-11-08 14:32:56 -0300

    Correção de erros

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Deu certo!');
+  res.status(200).send('Olá, Mundo!');
 });
 
 // Este 'if' evita que o app inicie durante os testes
