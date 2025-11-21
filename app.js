19aac3ccab4faf45b556d90e2107f2ccb58502f4
Maria Luiza Pestana <pestanamaria099@gmail.com>
2025-11-21 09:27:01 -0300

    Atualização no teste

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Olá, Mundo!');
+  res.status(200).send('Desafio de infraestrutura concluído!');
 });
 
 // Este 'if' evita que o app inicie durante os testes
