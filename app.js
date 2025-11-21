3f2a631b2f25ffd857d347e276739337a973a2f6
Maria Luiza Pestana <pestanamaria099@gmail.com>
2025-11-21 09:46:15 -0300

    Finalização infraestrutura!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Desafio de infraestrutura concluído');
+  res.status(200).send('Desafio de infraestrutura concluído com sucesso');
 });
 
 // Este 'if' evita que o app inicie durante os testes
--- app.test.js
+++ app.test.js
@@ -2,9 +2,9 @@
 const app = require('./app'); // Importa app
 
 describe('API Test', () => {
-  it('deve retornar "Desafio de infraestrutura concluído" na rota principal', async () => {
+  it('deve retornar "Desafio de infraestrutura concluído com sucesso" na rota principal', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Desafio de infraestrutura concluído');
+    expect(res.text).toBe('Desafio de infraestrutura concluído com sucesso');
   });
 });
